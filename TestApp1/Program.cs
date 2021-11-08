0c8789cb31ba1fec523944119693372d6b9c633e
YuriyFedosov <92025361+YuriyFedosov@users.noreply.github.com>
2021-11-08 23:31:51 +0300

    Добавьте файлы проекта.

---
+++ TestApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace TestApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.    Необходимо разработать программу для записи информации о товаре в текстовый файл в формате json.
+
+            Разработать класс для моделирования объекта «Товар». Предусмотреть члены класса «Код товара» (целое число), 
+                «Название товара» (строка), «Цена товара» (вещественное число).
+            Создать массив из 5-ти товаров, значения должны вводиться пользователем с клавиатуры.
+            Сериализовать массив в json-строку, сохранить ее программно в файл «Products.json».*/
+
+            int n = 5;
+            string path = "Products.json";
+            Product[] product = new Product[n];
+          
+            for (int i = 0; i < n; i++) //Заводим Позиции (Не русифицировал - для считывания назад в программе 2 некритично)
+            {
+                product[i] = new Product(); //Нужно определять каждый раз, иначе исключение
+                Console.Write("Создайте код {0} товара: ", i + 1);
+                product[i].Code = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Заполните наименование товара: ");
+                product[i].Name = Console.ReadLine();
+                Console.Write("Введите стоимость товара: ");
+                product[i].Price = Convert.ToDouble(Console.ReadLine());
+                Console.Write("\a");
+                Console.Clear();
+
+            }
+         
+            using (StreamWriter sw = new StreamWriter(path,false)) //Перезаписываем файл, если есть
+            {
+                for (int k = 0; k < n; k++)
+                {
+                        string jsonstring = JsonSerializer.Serialize(product[k]);
+                    if (k==n-1) // Проверка чтобы не ставил пустую последнюю строку в файле, которая дает исключение в программе 2
+                    {
+                        sw.Write(jsonstring);
+                    }
+                    else
+                    {
+                        sw.WriteLine(jsonstring);
+                    }
+                        
+                }
+                sw.Close();
+            }
+           
+            Console.Write("Данные сохранены в файл {0}", path);
+            Console.ReadKey();
+        }
+
+    }
+    class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ TestApp2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace TestApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2.    Необходимо разработать программу для получения информации о товаре из json-файла.
+                    Десериализовать файл «Products.json» из задачи 1. Определить название самого дорогого товара.*/
+
+
+            
+            string path = "C:\\Users\\Yuri Fedosov\\Desktop\\ITMO-BIM\\C#\\Projects\\Lab16\\TestApp1\\bin\\Debug\\Products.json"; //Тут путь в каталог с 1 программой
+            int n = 0;
+            double mPrice = 0;
+            string mName = "";
+            string[] jsString;
+            using (StreamReader sRead = new StreamReader(path))
+            {
+                jsString = sRead.ReadToEnd().Split('\n');
+
+            }
+            n = jsString.Length; // считаем строки
+            Product[] product = new Product[n];
+            for (int i = 0; i < n; i++)
+            {
+                
+                    product[i] = JsonSerializer.Deserialize<Product>(jsString[i]);// Считываем из файла
+               
+            }
+            foreach (Product a in product) //Выбираем максимальную
+            {
+                if (mPrice < a.Price)
+                {
+                    mPrice = a.Price;
+                    mName = a.Name;
+                }
+            }
+
+            Console.WriteLine("Самый дорогой товар \"{0}\" по цене {1}", mName, mPrice);
+            Console.ReadKey();
+        }
+    }
+    class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
